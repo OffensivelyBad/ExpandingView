35e029e3cf856391269e83a3b60a816ad373d640
SRoller <sroller@justfab.com>
2017-03-23 12:35:01 -0400

    initial commit

--- ExpandingView/ViewController.swift
+++ ExpandingView/ViewController.swift
@@ -9,17 +9,67 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    enum PopupStates {
+        case visible, hidden
+    }
+    var popupState = PopupStates.hidden
+    let popupDuration: Double = 0.1
+    let smallScale:CGFloat = 0.0001
+    let largeScale: CGFloat = 1
+    
+    @IBOutlet weak var popupView: UIView!
+    @IBOutlet weak var showScrollViewButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        initialSetup()
+    }
+    
+    func initialSetup() {
+        
+        //hide the popup view initially
+        self.popupView.transform = CGAffineTransform(scaleX: self.smallScale, y: self.largeScale)
+        self.popupView.isHidden = true
+        
+        self.popupView.layer.cornerRadius = 10
+        self.popupView.clipsToBounds = true
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func scrollViewButtonTapped(_ sender: Any) {
+        
+        var scale: CGFloat
+        
+        switch self.popupState {
+        case .visible:
+            //prepare variables for animation
+            scale = self.smallScale
+            self.popupState = .hidden
+            
+            //set the new button title
+            self.showScrollViewButton.titleLabel?.text = "Show Popup"
+        case .hidden:
+            //prepare variables for animation
+            scale = self.largeScale
+            self.popupView.isHidden = false
+            self.popupState = .visible
+            
+            //set the new button title
+            self.showScrollViewButton.titleLabel?.text = "Hide Popup"
+        }
+        
+        UIView.animate(withDuration: self.popupDuration, animations: { 
+            
+            self.popupView.transform = CGAffineTransform(scaleX: scale, y: scale)
+            
+        }) { (complete) in
+            
+            if self.popupState == .hidden { self.popupView.isHidden = true }
+            
+        }
+        
     }
-
-
+    
 }
 
